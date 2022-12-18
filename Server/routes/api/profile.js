9a5c328325ace251991b1af72fb3dbc798089d69
luanafandre <luh.franceschi@gmail.com>
2022-12-18 09:16:01 +0100

    hello

--- Server/routes/api/profile.js
+++ Server/routes/api/profile.js
@@ -6,9 +6,9 @@ const { check, validationResult } = require("express-validator");
 
 const normalize = require("normalize-url");
 const router = express.Router();
+const auth = require("../../middleware/auth.js");
 
 router.get("/", function (req, res) {
-  console.log("hello")
   res.json({
     message: "Hello from profile!",
   });
@@ -19,9 +19,10 @@ router.get("/", function (req, res) {
 // @access : private(needs token)/// validatin of token
 router.post(
   "",
+  auth,
   check("status", "status is required").notEmpty(),
   check("skills", "skills is required").notEmpty(),
-  async (req, res) => {
+    async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
